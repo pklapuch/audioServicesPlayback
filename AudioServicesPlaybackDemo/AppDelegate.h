595e26a47e02be12c26e9711a6b181e027c5f17b
pawel.klapuch <pawelklapuch@gmail.com>
2019-01-30 17:17:51 -0600

    Added demo app

---
+++ AudioServicesPlaybackDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioServicesPlaybackDemo
+//
+//  Created by Pawel Klapuch on 30/01/2019.
+//  Copyright © 2019 Pawel Klapuch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioServicesPlaybackDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioServicesPlaybackDemo
+//
+//  Created by Pawel Klapuch on 30/01/2019.
+//  Copyright © 2019 Pawel Klapuch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
