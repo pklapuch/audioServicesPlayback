527b62974edf380764620bcfe4e8ec07070fc153
pawel.klapuch <pawel.klapuch@spot.pl>
2019-05-08 20:28:35 -0500

    [DEV] copyrights

--- AudioServicesPlayback/Playback.m
+++ AudioServicesPlayback/Playback.m
@@ -3,7 +3,7 @@
 //  testAudio
 //
 //  Created by Pawel Klapuch on 30/01/2019.
-//  Copyright © 2019 Bosch. All rights reserved.
+//  Copyright © 2019 Pawel Klapuch. All rights reserved.
 //
 
 #import "Playback.h"
